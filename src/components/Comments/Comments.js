49dccaeb133c827fc9be20633d403c38fef3849d
Kyle Naumann <67936151+kyle-naumann@users.noreply.github.com>
2020-09-18 13:02:46 -0700

    Just need to figure out how to get the comments to be on the posts.

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -8,9 +8,14 @@ const Comments = props => {
 
   return (
     <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(comment => {
+        return <Comment coment ={comment}/>
+      }
+    )
+  };
     </div>
   );
-};
+}
+;
 
 export default Comments;
